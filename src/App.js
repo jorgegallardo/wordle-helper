8346d1d74876ac9b3b568e5fedc4f2052be0d439
Jorge Gallardo <jorge8486@gmail.com>
2022-02-11 23:29:57 -0500

    added list of sorted unique words

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { sortedUniqueWords } from './words';
+
 const App = () => {
-  return <div>Word Search</div>;
+  return (
+    <>
+      <h1>Word Search</h1>
+      <h3>To filter the list, enter only 5 letter words:</h3>
+      <input type="text" placeholder="word 1"></input>
+      <input type="text" placeholder="word 2"></input>
+      <input type="text" placeholder="word 3"></input>
+      <input type="text" placeholder="word 4"></input>
+      <input type="text" placeholder="word 5"></input>
+
+      <h3>Remaining Words:</h3>
+      <ol>
+        {sortedUniqueWords.map((word) => (
+          <li>{word}</li>
+        ))}
+      </ol>
+    </>
+  );
 };
 
 export default App;

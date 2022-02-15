565f84e0ce7730541afa9e545e522694568a576e
Jorge Gallardo <jorge8486@gmail.com>
2022-02-14 23:07:05 -0500

    filter list for words containing input

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
+import { useState } from 'react';
 import { sortedUniqueWords } from './words';
 
 const App = () => {
+  const [remainingWords, setRemainingWords] = useState(sortedUniqueWords);
+  const [firstWord, setFirstWord] = useState('');
+  const [secondWord, setSecondWord] = useState('');
+  const [thirdWord, setThirdWord] = useState('');
+  const [fourthWord, setFourthWord] = useState('');
+  const [fifthWord, setFifthWord] = useState('');
+
+  const filterRemainingWords = (event) => {
+    setFirstWord(event.target.value);
+  };
+
   return (
     <>
       <h1>Word Search</h1>
       <h3>To filter the list, enter only 5 letter words:</h3>
-      <input type="text" placeholder="word 1"></input>
-      <input type="text" placeholder="word 2"></input>
-      <input type="text" placeholder="word 3"></input>
-      <input type="text" placeholder="word 4"></input>
-      <input type="text" placeholder="word 5"></input>
+      <input
+        type="text"
+        placeholder="word 1"
+        maxLength={5}
+        onChange={filterRemainingWords}
+        value={firstWord}
+      ></input>
+      {/* <input type="text" placeholder="word 2" maxLength={5}></input>
+      <input type="text" placeholder="word 3" maxLength={5}></input>
+      <input type="text" placeholder="word 4" maxLength={5}></input>
+      <input type="text" placeholder="word 5" maxLength={5}></input> */}
 
-      <h3>Remaining Words:</h3>
+      <h3>
+        Remaining Words:{' '}
+        {remainingWords.filter((word) => word.includes(firstWord)).length}
+      </h3>
       <ol>
-        {sortedUniqueWords.map((word) => (
-          <li>{word}</li>
-        ))}
+        {remainingWords
+          .filter((word) => word.includes(firstWord))
+          .map((word, index) => (
+            <li key={index}>{word}</li>
+          ))}
       </ol>
     </>
   );
